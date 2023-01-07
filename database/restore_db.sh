e3dd43d73f6672c0c96359dc5e599815fd41f6bf
ThanhNguyenDat <johnkelvin3107@gmail.com>
2023-01-07 19:35:26 +0700

    update db

--- database/restore_db.sh
+++ database/restore_db.sh
@@ -1,2 +1,10 @@
+if [ "$( sudo -i -u postgres psql -XtAc "SELECT 1 FROM pg_database WHERE datname='ZATools'" )" = '1' ]
+then
+    echo "Database already exists"
+else
+    echo "Database does not exist"
+    echo "Creating database"
+    sudo -i -u postgres createdb ZATools
+fi
 
 pg_restore -d ZATools -h 127.0.0.1 -U toolslabel ZATools.dump
